51b6124b77aeb51b38c6842b82a8d9e3ba347c94
uttamprasad <uttam.kumar@collegedekho.com>
2021-03-06 13:59:30 +0530

    get value

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App =()=> {
+
+  const [number1, setNumber1] =useState('');
+  const [number2, setNumber2] =useState('');
+  const [total, setTotal] =useState('');
+
+  const getData =(event)=>{
+    setNumber1(+event.target.value);
+  }
+
+   const getData1 =(event)=>{
+    setNumber2(+event.target.value);
+  }
+
+  const handlerEvent = (event)=>{
+    event.preventDefault();
+    setTotal(number1 + number2);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>React Addition of Two Numbers </h2>
+    <form onSubmit={handlerEvent}>
+    <div><label>Value 1</label><input type="text" value={number1} onChange={getData}/></div>
+    <div><label>Value 2</label><input type="text" value={number2} onChange={getData1}/></div>
+    <button type="submit">Total Value</button>
+    </form>
+      <h2>{total}</h2>
     </div>
   );
 }
